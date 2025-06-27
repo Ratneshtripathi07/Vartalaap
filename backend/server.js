34c467362b037383eb442b24327078e4d3f0fef8
Ratneshtripathi07 <ratneshgeetatripathi@gmail.com>
2025-06-27 17:06:57 +0530

    backend connection on render

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://vartalaap-uuln.onrender.com"],
     credentials: true,
   })
 );
